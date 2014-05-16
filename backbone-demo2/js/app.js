aa6dc19d4f046fa400db7bc2cd356bfbf02d5b52
Dan Johnson <dan@balancerockmedia.com>
2014-05-16 08:22:23 -0400

    edits

--- backbone-demo2/js/app.js
+++ backbone-demo2/js/app.js
@@ -1,79 +1,51 @@
 // create backbone collection
 var Locations = Backbone.Collection.extend({
-    url: 'http://166.78.112.120:3000/locations' // or your local version
+    url: 'http://localhost:3000/locations' // or your local version
 });
 
 // create backbone collection
 var Currencies = Backbone.Collection.extend({
-    url: 'http://166.78.112.120:3000/currencies'
+    url: 'http://localhost:3000/currencies'
 });
 
 // page view
-var LocationPageView = Backbone.View.extend({
+var PageView = Backbone.View.extend({
     tagName: 'section',
     
-    template: Handlebars.compile($('#location-page-template').html()),
-    
-    render: function() {
-        this.$el.html(this.template());
+    initialize: function(options) {
+        this.page = options.page;
         
-        // add child view
-        var locationsView = new LocationsView({
-            collection: new Locations()
-        });
-        
-        this.$el.append(locationsView.render().el);
-        
-        return this;
-    }
-});
-
-var CurrencyPageView = Backbone.View.extend({
-    tagName: 'section',
-    
-    template: Handlebars.compile($('#currency-page-template').html()),
-    
-    render: function() {
-        this.$el.html(this.template());
-        
-        // add child view
-        var locationsView = new CurrencyView({
-            collection: new Currencies()
-        });
-        
-        this.$el.append(locationsView.render().el);
-        
-        return this;
-    }
-});
-
-// locations view
-var LocationsView = Backbone.View.extend({
-    tagName: 'ul',
-    
-    // constructor that adds a reset event to collection and then does a fetch
-    initialize: function() {
-        this.listenTo(this.collection, 'reset', this.render);
-        
-        this.collection.fetch({reset: true}); // reset true unless you want to merge items into collection
+        this.template = Handlebars.compile($('#'+this.page+'-page-template').html());
     },
     
     render: function() {
-        var models = this.collection.models;
+        this.$el.html(this.template());
         
-        // render each location model using a template
-        for (var i = 0; i < models.length; i++) {
-            var template = Handlebars.compile($('#location-template').html());
-            
-            this.$el.append(template(models[i].attributes));
+        var listView;
+        
+        // add child view
+        switch(this.page) {
+            case 'location':
+            listView = new ListView({
+                collection: new Locations()
+            });
+            break;
+        
+            case 'currency':
+            listView = new ListView({
+                collection: new Currencies()
+            });
+            break;
         }
         
+        this.$el.append(listView.render().el);
+        
         return this;
     }
 });
 
-// currencies view
-var CurrencyView = Backbone.View.extend({
+// currency view
+var ListView = Backbone.View.extend({
     tagName: 'ul',
     
     // constructor that adds a reset event to collection and then does a fetch
@@ -88,7 +60,7 @@ var CurrencyView = Backbone.View.extend({
         
         // render each location model using a template
         for (var i = 0; i < models.length; i++) {
-            var template = Handlebars.compile($('#currency-template').html());
+            var template = Handlebars.compile($('#list-template').html());
             
             this.$el.append(template(models[i].attributes));
         }
@@ -101,23 +73,28 @@ var CurrencyView = Backbone.View.extend({
 var Router = Backbone.Router.extend({
     routes: {
         '': 'locations',
+        'locations': 'locations',
         'currencies': 'currencies'
     },
     
     locations: function() {
         // create an instance of the PageView
-        var locationPageView = new LocationPageView();
+        var pageView = new PageView({
+            page: 'location'
+        });
         
         // render the page view
-        $('#container').html(locationPageView.render().el);
+        $('#container').html(pageView.render().el);
     },
     
     currencies: function() {
         // create an instance of the PageView
-        var currencyPageView = new CurrencyPageView();
+        var pageView = new PageView({
+            page: 'currency'
+        });
         
         // render the page view
-        $('#container').html(currencyPageView.render().el);
+        $('#container').html(pageView.render().el);
     }
 });
 
